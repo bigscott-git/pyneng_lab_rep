add93470e268c4ebe377c554505b6c0041b37625
bigscott-git <i-a-orlov-89@yandex.ru>
2020-01-04 08:55:59 -0600

    9.3a done

--- 09_functions/task_9_3a.py
+++ 09_functions/task_9_3a.py
@@ -1,25 +1,26 @@
-# -*- coding: utf-8 -*-
-'''
-Задание 9.3a
+#!/usr/bin/env python3
 
-Сделать копию функции get_int_vlan_map из задания 9.3.
+def get_int_vlan_map(config_filename):
+	with open(config_filename, 'r') as source:
+		result = []
+		access_dict = {}
+		trunk_dict = {}
+		for lines in source:
+			if 'FastEthernet' in lines:
+				interface = lines.split()[-1]
+			elif 'mode access' in lines:
+				access_dict[interface] = 1
+			elif 'access vlan' in lines:
+				access_dict[interface] = int(lines.split()[-1])
+			elif 'allowed vlan' in lines:
+				trunk_dict[interface] = [int(v) for v in str.split(lines.split()[-1], ',')]			
+	
+		result.append(access_dict)
+		result.append(trunk_dict)
+		
+	print(tuple(result))
+	return(tuple(result))
 
-Дополнить функцию:
-    - добавить поддержку конфигурации, когда настройка access-порта выглядит так:
-            interface FastEthernet0/20
-                switchport mode access
-                duplex auto
-      То есть, порт находится в VLAN 1
-
-В таком случае, в словарь портов должна добавляться информация, что порт в VLAN 1
-      Пример словаря: {'FastEthernet0/12': 10,
-                       'FastEthernet0/14': 11,
-                       'FastEthernet0/20': 1 }
-
-У функции должен быть один параметр config_filename, который ожидает как аргумент имя конфигурационного файла.
-
-Проверить работу функции на примере файла config_sw2.txt
+get_int_vlan_map('config_sw2.txt')
 
 
-Ограничение: Все задания надо выполнять используя только пройденные темы.
-'''
