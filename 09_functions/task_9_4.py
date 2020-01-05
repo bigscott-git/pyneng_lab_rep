07d82f54df29bc0acd346e2d5e3eeeb75e65d9af
bigscott-git <i-a-orlov-89@yandex.ru>
2020-01-05 08:04:04 -0600

    09_functions is done

--- 09_functions/task_9_4.py
+++ 09_functions/task_9_4.py
@@ -1,28 +1,6 @@
-# -*- coding: utf-8 -*-
-'''
-Задание 9.4
-
-Создать функцию convert_config_to_dict, которая обрабатывает конфигурационный файл коммутатора и возвращает словарь:
-* Все команды верхнего уровня (глобального режима конфигурации), будут ключами.
-* Если у команды верхнего уровня есть подкоманды, они должны быть в значении у соответствующего ключа, в виде списка (пробелы в начале строки надо удалить).
-* Если у команды верхнего уровня нет подкоманд, то значение будет пустым списком
-
-У функции должен быть один параметр config_filename, который ожидает как аргумент имя конфигурационного файла.
-
-Проверить работу функции на примере файла config_sw1.txt
-
-При обработке конфигурационного файла, надо игнорировать строки, которые начинаются с '!',
-а также строки в которых содержатся слова из списка ignore.
-
-Для проверки надо ли игнорировать строку, использовать функцию ignore_command.
-
-
-Ограничение: Все задания надо выполнять используя только пройденные темы.
-'''
+#!/usr/bin/env python3
 
 ignore = ['duplex', 'alias', 'Current configuration']
-
-
 def ignore_command(command, ignore):
     '''
     Функция проверяет содержится ли в команде слово из списка ignore.
@@ -35,3 +13,23 @@ def ignore_command(command, ignore):
     * False - если нет
     '''
     return any(word in command for word in ignore)
+
+
+def convert_config_to_dict(config_filename,):
+	with open(config_filename, 'r') as source:
+		result = {}
+		for lines in source:
+			if ignore_command(lines, ignore):
+				continue
+			elif lines.startswith('!') or lines.startswith('\n'):
+				continue
+			elif lines[0] != ' ':
+				keys = lines.strip()
+				result[keys] = []
+			elif lines.startswith(' ') and lines[1] != ' ':
+				result[keys].append(lines.strip())
+	print(result)
+	return(result)
+
+
+convert_config_to_dict('config_sw1.txt')
