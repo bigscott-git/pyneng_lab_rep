fbb209061ed9cc82174afc4e157b1b26ac9791de
bigscott-git <i-a-orlov-89@yandex.ru>
2020-01-21 08:58:38 -0600

    EGISZ

--- EGISZ/fw_migration.py
+++ EGISZ/fw_migration.py
@@ -3,7 +3,7 @@
 import json
 from pprint import pprint
 
-with open('exported_data.xml', 'r') as source, open('fw_result.txt', 'w') as dest:
+with open('exported_data.xml', 'r') as source, open('result.txt', 'w') as dest:
         dict = {}
         for lines in source:
                 if '<rule_entry comment' in lines:
@@ -12,14 +12,14 @@ with open('exported_data.xml', 'r') as source, open('fw_result.txt', 'w') as des
                         policy = lines[lines.index('tag=') + 5 : lines.index('>') - 3]
                         dict[policy] = {}
                 elif '<match_source_ref' in lines:
-                        dict[policy]['srcaddr'] = lines[lines.index('value="') + 7 : lines.index('"/>')]
+                        dict[policy]['srcaddr'] = lines[lines.index('value=') + 7 : lines.index('/>') - 1]
                 elif '<match_destination_ref' in lines:
-                        dict[policy]['dstaddr'] = lines[lines.index('value="') + 7 : lines.index('"/>')]
+                        dict[policy]['dstaddr'] = lines[lines.index('value=') + 7 : lines.index('/>') - 1]
                 elif '<match_service_ref' in lines:
-                        dict[policy]['service'] = lines[lines.index('value="') + 7 : lines.index('"/>')]
+                        dict[policy]['service'] = lines[lines.index('value=') + 7 : lines.index('/>') - 1]
                 elif '<action type=' in lines:
-                        dict[policy]['action'] = lines[lines.index('type="') + 6 : lines.index('"/>')]
+                        dict[policy]['action'] = lines[lines.index('type=') + 6 : lines.index('/>') - 1]
                                
         dest.write(json.dumps(dict))
-        pprint(json.dumps(dict))
+#        pprint(json.dumps(dict))
 
